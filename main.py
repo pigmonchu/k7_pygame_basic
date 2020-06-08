48fd718844ed4ae925b8f34a1f27baa790bebc3f
Marisa Domingo <mluisadomingo@MacBook-Pro-de-Marisa.local>
2020-06-08 22:14:29 +0200

    Bucle control GUI basico

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import * 
+import sys
+
+pygame.init()
+
+pantalla =  pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Hola Mundo")
+
+rojo = 0
+direccion = 1
+
+juego_activo = True
+
+while juego_activo:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            juego_activo = False
+    if rojo >= 255:
+        direccion = -1
+
+    if rojo <= 0:
+        direccion = 1
+
+    rojo += direccion
+
+    pantalla.fill((rojo, 0, 0))
+
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+
+pygame.quit()
+sys.exit()
