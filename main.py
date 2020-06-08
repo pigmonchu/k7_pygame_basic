d3e40d3fa341865a7532f35c134a468fae7376a8
Marisa Domingo <mluisadomingo@MacBook-Pro-de-Marisa.local>
2020-06-08 23:40:48 +0200

    Posicionando la bola

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ from pygame.locals import *
 import sys
 
 BACKGROUND = (0,240,0)
+YELLOW = (255, 255, 0)
+class Ball: 
+    def __init__(self):
+        self.vx = 5
+        self.vy = 5
+        self.Cx = 400
+        self.Cy = 300
+        self.h = 20
+        self.w = 20
+        self.posx = self.Cx - self.w // 2
+        self.posy = self.Cy - self.h // 2
+
+
+        self.image = pg.Surface((20, 20))
+        self.image.fill(YELLOW)
 
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode((800, 600))
         self.pantalla.fill(BACKGROUND)
+        fondo = pg.image.load("./resources/images/fondo.jpg")
+        self.pantalla.blit(fondo, (0, 0))
+        self.ball = Ball()
+
         pg.display.set_caption("Pong")
 
     def main_loop(self):
@@ -18,6 +37,7 @@ class Game:
                 if event.type == QUIT:
                     game_over = True
 
+            self.pantalla.blit(self.ball.image, (self.ball.posx, self.ball.posy))
             pg.display.flip()
 
     def quit(self):
