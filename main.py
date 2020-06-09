9e3edb7031dc8eeb8e976c41dc5219a99ecc03a5
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-06-09 23:40:04 +0200

    Comenzando con fuentes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame as pg
 from pygame.locals import *
-import sys
+import sys, random
 
 BACKGROUND = (50,50,50)
 YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+WIN_GAME_SCORE = 10
+
 class Ball: 
     def __init__(self):
-        self.vx = 5
-        self.vy = 5
-        self.Cx = 400
-        self.Cy = 300
+        self.reset()
         self.h = 20
         self.w = 20
 
@@ -26,7 +27,8 @@ class Ball:
 
     def move(self, limSupX, limSupY):
         if self.Cx >= limSupX or self.Cx <=0:
-            self.vx *= -1
+            self.vx = 0
+            self.vy = 0
 
         if self.Cy >= limSupY or self.Cy <=0:
             self.vy *= -1
@@ -42,6 +44,12 @@ class Ball:
             self.vx *= -1
             self.Cx += self.vx
             self.Cy += self.vy
+
+    def reset(self):
+        self.vx = random.choice([-7, -5, 5, 7])
+        self.vy = random.choice([-7, -5, 5, 7]) 
+        self.Cx = 400
+        self.Cy = 300
 
 class Raquet:
     def __init__(self, Cx):
@@ -83,50 +91,79 @@ class Game:
         self.ball = Ball()
         self.playerOne = Raquet(30)
         self.playerTwo = Raquet(770)
+
+        self.font = pg.font.Font('./resources/fonts/PressStart2P-Regular.ttf', 36)
+
+        self.marcadorOne = self.font.render("0", True, WHITE)
+        self.marcadorTwo = self.font.render("0", True, WHITE)
+
+        self.scoreOne = 0
+        self.scoreTwo = 0
         pg.display.set_caption("Pong")
+
+
+    def handlenEvent(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                return True
+            '''
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    self.playerOne.vy = -5
+
+
+                if event.key == K_DOWN:
+                    self.playerOne.vy = 5
+            '''
+        key_pressed = pg.key.get_pressed()
+        if key_pressed[K_UP]:
+            self.playerTwo.vy = - 5
+        elif key_pressed[K_DOWN]:
+            self.playerTwo.vy = 5
+        else:
+            self.playerTwo.vy = 0
+
+        if key_pressed[K_w]:
+            self.playerOne.vy = - 5
+        elif key_pressed[K_z]:
+            self.playerOne.vy = 5
+        else:
+            self.playerOne.vy = 0
+        
+        return False
 
     def main_loop(self):
         game_over = False
 
         while not game_over:
-            for event in pg.event.get():
-                if event.type == QUIT:
-                    game_over = True
-                '''
-                if event.type == KEYDOWN:
-                    if event.key == K_UP:
-                        self.playerOne.vy = -5
 
-
-                    if event.key == K_DOWN:
-                        self.playerOne.vy = 5
-                '''
-            key_pressed = pg.key.get_pressed()
-            if key_pressed[K_UP]:
-                self.playerTwo.vy = - 5
-            elif key_pressed[K_DOWN]:
-                self.playerTwo.vy = 5
-            else:
-                self.playerTwo.vy = 0
-
-            if key_pressed[K_w]:
-                self.playerOne.vy = - 5
-            elif key_pressed[K_z]:
-                self.playerOne.vy = 5
-            else:
-                self.playerOne.vy = 0
-
-
-            self.pantalla.blit(self.fondo, (0, 0))
-            self.pantalla.blit(self.ball.image, (self.ball.posx, self.ball.posy))
-            self.pantalla.blit(self.playerOne.image, (self.playerOne.posx, self.playerOne.posy))
-            self.pantalla.blit(self.playerTwo.image, (self.playerTwo.posx, self.playerTwo.posy))
+            game_over = self.handlenEvent()
 
             self.ball.move(800, 600)
             self.playerOne.move(800, 600)
             self.playerTwo.move(800, 600)
             self.ball.comprobarChoque(self.playerOne)
             self.ball.comprobarChoque(self.playerTwo)
+
+            if self.ball.vx == 0 and self.ball.vy == 0:
+                if self.ball.Cx >=800:
+                    self.scoreOne += 1
+                if self.ball.Cx <= 0:
+                    self.scoreTwo += 1
+
+                if self.scoreOne == 10 or self.scoreTwo == 10:
+                    game_over = True
+
+                self.ball.reset()
+
+
+
+            self.pantalla.blit(self.fondo, (0, 0))
+            self.pantalla.blit(self.ball.image, (self.ball.posx, self.ball.posy))
+            self.pantalla.blit(self.playerOne.image, (self.playerOne.posx, self.playerOne.posy))
+            self.pantalla.blit(self.playerTwo.image, (self.playerTwo.posx, self.playerTwo.posy))
+            self.pantalla.blit(self.marcadorOne, (10, 10))
+            self.pantalla.blit(self.marcadorTwo, (740, 10))
 
             pg.display.flip()
 
