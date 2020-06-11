fc826da0a2678ca0b1ec1f528fc7b9fa5bfe25e9
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-06-12 00:32:30 +0200

    Y fin

--- main_with_sprites.py
+++ main_with_sprites.py
@@ -16,8 +16,16 @@ class Game:
         self.pantalla.fill(BACKGROUND)
         self.fondo = pg.image.load("./resources/images/fondo.jpg")
         self.ball = Ball()
+
         self.playerOne = Raquet(30)
         self.playerTwo = Raquet(770)
+        self.playersGroup = pg.sprite.Group()
+        self.playersGroup.add(self.playerOne)
+        self.playersGroup.add(self.playerTwo)
+
+        self.allSprites = pg.sprite.Group()
+        self.allSprites.add(self.ball)
+        self.allSprites.add(self.playersGroup)
 
         self.status = 'Partida'
 
@@ -80,11 +88,9 @@ class Game:
         while not game_over:
             game_over = self.handlenEvent()
 
-            self.ball.update(800, 600)
-            self.playerOne.update(600)
-            self.playerTwo.update(600)
-            self.ball.comprobarChoque(self.playerOne)
-            self.ball.comprobarChoque(self.playerTwo)
+            self.allSprites.update(800, 600)
+
+            self.ball.comprobarChoque(self.playersGroup)
 
             if self.ball.vx == 0 and self.ball.vy == 0:
                 if self.ball.rect.centerx >=800:
@@ -100,9 +106,7 @@ class Game:
                 self.ball.reset()
 
             self.pantalla.blit(self.fondo, (0, 0))
-            self.pantalla.blit(self.ball.image, (self.ball.rect.x, self.ball.rect.y))
-            self.pantalla.blit(self.playerOne.image, (self.playerOne.rect.x, self.playerOne.rect.y))
-            self.pantalla.blit(self.playerTwo.image, (self.playerTwo.rect.x, self.playerTwo.rect.y))
+            self.allSprites.draw(self.pantalla)
             self.pantalla.blit(self.marcadorOne, (30, 10))
             self.pantalla.blit(self.marcadorTwo, (740, 10))
 
--- sprites.py
+++ sprites.py
@@ -12,27 +12,33 @@ WIN_GAME_SCORE = 3
 class Ball(pg.sprite.Sprite):
     vx = 0
     vy = 0
-    __color = WHITE
+    num_sprites = 12
 
     def __init__(self):
-        self.image = pg.Surface((20, 20))
-        self.color = YELLOW
-        self.image.fill(self.__color)
+        super().__init__()
+        self.image = pg.Surface((20, 20), pg.SRCALPHA, 32)
         self.rect = self.image.get_rect()
+        self.images = self.loadImages()
+        self.image_act = 0
+        self.image.blit(self.images[self.image_act], (0, 0))
         self.reset()
 
+        self.soyToxica = False
+        
         self.ping = pg.mixer.Sound('./resources/sounds/ping.wav')
         self.lost_point = pg.mixer.Sound('./resources/sounds/lost-point.wav')
 
+    def loadImages(self):
+        '''
+        images = []
+        for i in range(self.num_sprites):
+            image = pg.image.load("./resources/sprites/f_{}.png".format(i))
+            images.append(image)
+        return images
+        '''
+        return [pg.image.load("./resources/sprites/f_{}.png".format(i)) for i in range(self.num_sprites)]
+        
 
-    @property
-    def color(self):
-        return self.__color
-
-    @color.setter
-    def color(self, tupla_color):
-        self.__color = tupla_color
-        self.image.fill(self.__color)
 
     def reset(self):
         self.vx = random.choice([-7, -5, 5, 7])
@@ -40,6 +46,17 @@ class Ball(pg.sprite.Sprite):
         self.rect.centerx = 400
         self.rect.centery = 300
 
+    def comprobarChoque(self, group):
+        lista_candidatos = pg.sprite.spritecollide(self, group, False)
+        if len(lista_candidatos) > 0:
+            self.vx *= -random.uniform(0.8, 1.3)
+            self.vy *= random.uniform(0.8, 1.3)
+
+            self.rect.centerx += self.vx
+            self.rect.centery += self.vy
+            self.ping.play()
+
+    '''
     def comprobarChoque(self, something):
         dx = abs(self.rect.centerx - something.rect.centerx)
         dy = abs(self.rect.centery - something.rect.centery)
@@ -51,7 +68,7 @@ class Ball(pg.sprite.Sprite):
             self.rect.centerx += self.vx
             self.rect.centery += self.vy
             self.ping.play()
-
+    '''
     def update(self, limSupX, limSupY):
         if self.rect.centerx >= limSupX or self.rect.centerx <=0:
             self.vx = 0
@@ -65,17 +82,32 @@ class Ball(pg.sprite.Sprite):
         self.rect.centerx += self.vx
         self.rect.centery += self.vy
 
+        #animar bola
+        '''
+        self.image_act += 1
+        if self.image_act >= self.num_sprites:
+            self.image_act = 0
+
+        self.image_act +=1
+        self.image_act = self.image_act % self.num_sprites
+        '''
+
+        self.image_act = (self.image_act + 1) % self.num_sprites
+        self.image.blit(self.images[self.image_act], (0, 0))
+
 class Raquet(pg.sprite.Sprite):
     vx = 0
     vy = 0
     __color = WHITE
 
     def __init__(self, centerx):
+        super().__init__()
         self.image = pg.Surface((25, 100))
         self.image.fill(self.__color)
         self.rect = self.image.get_rect()
         self.rect.centerx = centerx
         self.rect.centery = 400
+        self.impacto = False
 
     @property
     def color(self):
@@ -87,7 +119,7 @@ class Raquet(pg.sprite.Sprite):
         self.image.fill(self.__color)
 
 
-    def update(self, limSupY):
+    def update(self, limSupX, limSupY):
         self.rect.centerx += self.vx
         self.rect.centery += self.vy
 
